8a5c7453f016d0277cde72f0507c9dcd802a0d24
fysteven <yaofan@usc.edu>
2015-10-18 22:05:07 -0700

    modify create_baseline_features and main

--- create_advanced_features.py
+++ create_advanced_features.py
@@ -1,12 +1,19 @@
 import sys
-from os import listdir
 from hw3_corpus_tool import get_utterances_from_file, get_utterances_from_filename
 
 __author__ = 'Frank'
 
 
-def convert_csv_into_format(path_to_file):
-    file = open(path_to_file)
+def need_to_filter(pos):
+    to_be_filtered = {'UH', ',', 'CC'}
+    if pos in to_be_filtered:
+        return True
+    else:
+        return False
+
+
+def create_features(file_name):
+    file = open(file_name)
     output = get_utterances_from_file(file)
     first_utterance_has_past = False
     previous_speaker = ''
@@ -21,7 +28,11 @@ def convert_csv_into_format(path_to_file):
 
         if pos_tag:
             for i in range(0, len(pos_tag)):
-                print('w[' + str(i) + ']=' + pos_tag[i][0] + '\tpos[' + str(i) + ']=' + pos_tag[i][1], end='\t')
+                # filter some words
+                if need_to_filter(pos_tag[i][1]):
+                    pass
+                else:
+                    print('w[' + str(i) + ']=' + pos_tag[i][0] + '\tpos[' + str(i) + ']=' + pos_tag[i][1], end='\t')
         if previous_speaker != line[1]:
             # the speaker of the current utterance has changed
             if previous_speaker != '':
@@ -30,26 +41,10 @@ def convert_csv_into_format(path_to_file):
         previous_speaker = line[1]
 
 
-def get_all_files_in_directory(directory):
-    files = []
-    for name in listdir(directory):
-        if name == '.DS_Store':
-            pass
-        else:
-            files.append(directory + name)
-    return files
-
-
-def function1(directory):
-    files = get_all_files_in_directory(directory)
-
-    for file in files:
-        convert_csv_into_format(file)
-
-
 def main():
     if len(sys.argv) >= 1:
-        convert_csv_into_format(sys.argv[1])
+        create_features(sys.argv[1])
+
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import random
 from create_baseline_features import convert_csv_into_format
+from create_advanced_features import create_features
 from os import listdir
 from hw3_corpus_tool import get_utterances_from_file, get_utterances_from_filename
 
@@ -22,6 +23,15 @@ def function1(directory):
 
     for file in files:
         convert_csv_into_format(file)
+
+
+def function2(directory):
+    files = get_all_files_in_directory(directory)
+
+    for file in files:
+        create_features(file)
+
+    return
 
 
 def split_data(input_file_name, percentage):
@@ -54,6 +64,37 @@ def split_data(input_file_name, percentage):
     output_dev_file.close()
 
 
+def split_data2(input_file_name, percentage):
+    input_file = open(input_file_name)
+    lines = []
+    for line in input_file:
+        if line == '\n':
+            pass
+        else:
+            lines.append(line)
+    line_count = len(lines)
+
+    training_line_numbers = random.sample(range(0, line_count), int(line_count * float(percentage) / 100))
+    set_training_line_numbers = set(training_line_numbers)
+    set_dev_line_numbers = {i for i in range(0, line_count)} - set_training_line_numbers
+    training_lines = ''
+    dev_lines = ''
+
+    for line in training_line_numbers:
+        training_lines += lines[line] + '\n'
+
+    output_training_file = open(input_file_name + '_' + percentage + '_training', 'w')
+    output_training_file.write(training_lines)
+    output_training_file.close()
+
+    for line in set_dev_line_numbers:
+        dev_lines += lines[line] + '\n'
+
+    output_dev_file = open(input_file_name + '_' + percentage + '_development', 'w')
+    output_dev_file.write(dev_lines)
+    output_dev_file.close()
+
+
 def calculate_accuracy(file_name):
     file = open(file_name)
     count_accurate = 0
@@ -72,22 +113,39 @@ def calculate_accuracy(file_name):
     return accuracy
 
 
+def show_different_labels(file_name):
+    file = open(file_name)
+    for line in file:
+        if line == '\n':
+            pass
+        else:
+            words = line.split()
+            if words[0] != words[1]:
+                print(line)
+    return
+
+
 def main():
     if len(sys.argv) == 1:
         program = 'python3 '
         current_script = sys.argv[0]
         print('how to use this program?')
         print(program + current_script + ' convert path_to_a_directory_of_csv_files')
+        print(program + current_script + ' convert_advanced path_to_a_directory_of_csv_files')
         print(program + current_script + ' split train.crfsuite.txt 75')
         print(program + current_script + ' calculate one_output_file_generated_by_CRFsuite')
     if len(sys.argv) >= 3:
         if sys.argv[1] == 'convert':
             function1(sys.argv[2])
+        elif sys.argv[1] == 'convert_advanced':
+            function2(sys.argv[2])
         elif sys.argv[1] == 'calculate':
             calculate_accuracy(sys.argv[2])
+        elif sys.argv[1] == 'diff':
+            show_different_labels(sys.argv[2])
     if len(sys.argv) >= 4:
         if sys.argv[1] == 'split':
-            split_data(sys.argv[2], sys.argv[3])
+            split_data2(sys.argv[2], sys.argv[3])
 
 
 main()
